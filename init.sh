ae364fe9c77e4b9ae8a6133090e81206fa2629a3
Brian Vallelunga <brian@laborate.io>
2013-09-05 20:09:30 -0700

    update

--- init.sh
+++ init.sh
@@ -34,6 +34,7 @@ echo -e '\033[32mRedis Install Complete \033[m'
 echo -e '\033[32mInstalling Node \033[m'
 apt-get -y nodejs
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+nvm install v0.10.0
 echo -e '\033[32mNode Install Complete \033[m'
 
 #Install Nodes Modules
