d63511b96394bbe4f317771b21e8435dba037c0d
Brian Vallelunga <brian@laborate.io>
2013-12-17 02:50:36 -0800

    Create post.sh

---
+++ post.sh
@@ -0,0 +1,21 @@
+#Start Up
+BASE="$(cd "$(dirname "$0")"; pwd)"
+
+#Set Node Version
+nvm install v0.10.7
+nvm use v0.10.7
+
+#Install Nodes Modules
+npm install express -g
+npm install forever -g
+npm install csslint -g
+npm install jslint -g
+npm install jsonlint -g
+
+#Clean Up
+chmod -R 777 /var/spool/cron
+chmod 751 /home
+mkdir /home/archived_users/
+rm -rf $BASE/base-init
+shutdown -r 0
+exit
