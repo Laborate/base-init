2781198668adf030fbdf2f8d32a8d408dfae5c12
Brian Vallelunga <brian@laborate.io>
2013-12-17 02:54:48 -0800

    Update post.sh

--- post.sh
+++ post.sh
@@ -12,10 +12,14 @@ npm install csslint -g
 npm install jslint -g
 npm install jsonlint -g
 
+#Remove Crontab Reboot
+touch $BASE/mycron;
+crontab $BASE/mycron;
+
 #Clean Up
 chmod -R 777 /var/spool/cron
 chmod 751 /home
 mkdir /home/archived_users/
-rm -rf $BASE/base-init
+rm -rf $BASE
 shutdown -r 0
 exit
